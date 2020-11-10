0c4bc5911435ec8db2f7a85f83736cd6ca3c127b
jokahata <jokahata@zynga.com>
2020-11-10 12:43:39 -0800

    Add zsh setup and themeing

--- dotfilesetup.sh
+++ dotfilesetup.sh
@@ -1,2 +1,19 @@
 
-brew cask install macvim
+if ! command mvim &> /dev/null; then
+				brew cask install macvim
+fi
+
+# Install zsh
+if [[ ! -d ~/.oh-my-zsh ]]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# Setup passion theme 
+brew install coreutils
+if [[ ! -f ~/.oh-my-zsh/custom/passion.zsh-theme ]]; then
+	git clone git@github.com:ChesterYue/ohmyzsh-theme-passion.git
+	cp ohmyzsh-theme-passion/passion.zsh-theme .oh-my-zsh/custom/passion.zsh-theme
+	rm -rf ./ohmyzsh-theme-passion
+fi
+
+
